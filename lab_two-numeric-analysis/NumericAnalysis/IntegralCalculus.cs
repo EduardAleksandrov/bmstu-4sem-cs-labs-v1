1f226131ff0bf31795ca3c3033626ca927e8236b
Eduard Aleksandrov <eduard.ale@list.ru>
2024-04-11 14:06:57 +0300

    copy project from task

---
+++ lab_two-numeric-analysis/NumericAnalysis/IntegralCalculus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumericAnalysis;
+
+public class IntegralCalculus
+{
+    public static double Calculate(Func<double, double> func, double x1, double x2, double precision)
+    {
+        double y1 = func(x1);
+        double y2 = func(x2);
+        return 2.0;
+
+        throw new NotImplementedException();
+    }
+}
