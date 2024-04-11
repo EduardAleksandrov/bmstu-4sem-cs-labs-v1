34bac95594ff1fa633478fbd5c59350947f305ac
Eduard Aleksandrov <eduard.ale@list.ru>
2024-04-11 15:18:13 +0300

    Этап 1

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -2,6 +2,26 @@
 {
     private static void Main(string[] args)
     {
-        // Console.WriteLine("Hello, World!");
+        for(;;)
+        {
+            Console.WriteLine("Начни печатать");
+            Console.ReadLine();
+
+            DateTime startedAt = DateTime.Now;
+
+            string txt1 = "Hello my dear friend!";
+            Console.WriteLine(txt1);
+
+            string? get_txt = Console.ReadLine();
+            TimeSpan span = DateTime.Now - startedAt;
+            Console.WriteLine($"Время печати {span}, результат хороший попробуйте еще раз? Да/Нет");
+
+            string? response = Console.ReadLine();
+            if(response != "Да") break; 
+
+
+
+        }
+        
     }
 }
